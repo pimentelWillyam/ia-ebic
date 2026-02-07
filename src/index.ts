35f1d7c7b6dffb8f30eb907c7f5a04cee5e5b4c1
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-02-07 20:29:02 -0300

    feat: second commit

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import app from "./api/app"
 
 app.listen(process.env.PORTA, () =>{
     console.log(`aplicação iniciada na porta ${process.env.PORTA}`)
+    console.log(`ROTAS: 
+        POST ${process.env.HOST}:${process.env.PORTA}/api/dialogue
+        `)
+
 })
