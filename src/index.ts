ada45449777131202a25120a4275928fa59e1166
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-02-09 15:07:04 -0300

    fix: corrigindo endereços de exemplo

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ app.listen(Config.API.PORT, () =>{
     console.log(`aplicação iniciada no endereço ${Config.API.HOST}:${Config.API.PORT}`)
     console.log(`ROTAS: 
         POST ${Config.API.HOST}:${Config.API.PORT}/api/dialogue
-        POST ${Config.API.HOST}:${Config.API.PORT}/api/ai
+        POST ${Config.API.HOST}:${Config.API.PORT}/api/ai/generate
         `)
-
 })
