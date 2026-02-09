b149dd50d00fc672f1bebf31c4603089523a287a
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-02-09 15:05:59 -0300

    fix: corrigindo config

--- src/Config.ts
+++ src/Config.ts
@@ -1,11 +1,11 @@
 export const Config = {
   AI: {
-    URL: process.env.OLLAMA_URL ?? "http://localhost:11434/api/generate",
-    MODEL: process.env.OLLAMA_MODEL ?? "qwen2.5:7b",
+    URL: process.env.IA_URL ?? "http://localhost:11434/api/generate",
+    MODEL: process.env.IA_MODEL ?? "qwen2.5:7b",
   },
   API: {
     HOST: process.env.HOST ?? "http://localhost",
-    PORT: process.env.PORTA ?? 3000,
+    PORT: process.env.PORT ?? 3000,
   }
   
 }
