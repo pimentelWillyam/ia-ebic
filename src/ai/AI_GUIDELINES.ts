c6ea1bf7656d53dc603b54a80fa89aae59a0d917
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-02-12 00:07:03 -0300

    feat: adicionando guidelines de ia

--- src/ai/AI_GUIDELINES.ts
+++ src/ai/AI_GUIDELINES.ts
@@ -26,5 +26,70 @@ Diretrizes obrigatórias:
 - Retorne exclusivamente o JSON válido.
 - Não inclua texto fora do JSON.
 
+6. Formato do JSON:
+
+    interface InformacoesVitima {      
+      identidadeDesconhecida: boolean,
+      nome: string
+      nomeMae: string
+      apelido: string
+      sexo: string
+      nomeSocial: string
+      dataNascimento: Date | null
+      idadeAparente: string
+      corPele: string
+      posicaoNaQualCorpoFoiEncontrado: string
+      tipoLocalMorte: string
+      corpoEstavaVestido: string
+      dataHoraObito: Date | null
+      dataHoraObitoDesconhecida: boolean
+      naturezaOcorrencia: string
+      intencao: string
+      objetosUtilizados: string
+      informacoesComplementaresCorpo: string  
+      examesNaoSaoSolicitados: boolean
+      exameSubungueal: boolean
+      coletaDNAReferencia: boolean
+      restosMortaisNaoIdentificados: boolean
+      toxicologico: boolean
+      vaginal: boolean
+      anal: boolean
+      residuografico: boolean
+      alcoolemia: boolean
+      outrosExames: boolean
+      descricaoOutrosExames: string
+      NIC: string
+      posicaoComoCorpoFoiEncontrado: string
+      imagemIdentificadoraCorpo: string
+      PICAplicada: string
+      documentoVitima: string
+      descrevaOcorrencia: string
+    }
+ 7. Valores possíveis:
+
+    identidadeDesconhecida é false se o nome da vítima for conhecido, true caso contrário
+    
+    sexo é MASCULINO, FEMININO ou DESCONHECIDO
+
+    corPele é AMARELA, BRANCA, INDÍGENA, NEGRA, PARDA ou DESCONHECIDA
+
+    posicaoNaQualCorpoFoiEncontrado é DORSAL, LATERAL DIREITO, LATERAL ESQUERDO, VENTRAL, OUTRO ou NÃO SE APLICA
+
+    tipoLocalMorte pode ser AREIA, ASFALTO, AÇUDE, BARRO, CANAVIAL, CIMENTO, LAGOA, LAMA, MANGUEZAL, MAR, MATA ou NÃO SE APLICA
+
+    corpoEstavaVestido pode ser SIM, NÃO, PARCIALMENTE ou NÃO SE APLICA
+
+    naturezaOcorrencia pode ser A ESCLARECER, ACIDENTE DE TRÂNSITO, HOMICÍDIO, LATROCÍNIO, LESÃO CORPORAL SEG. DE MORTE, SUICÍDIO, OUTROS ACIDENTES
+
+    objetosUtilizados são uma lista que pode conter ARMA BRANCA, ARMA DE FOGO, OBJETO CONTUNDENTE e NAO SE APLICA
+
+    descrevaOcorrencia é uma descrição do que ocorreu
+
+ abaixo virá o texto que deve ser usasdo como base para gerar o json:
+
 
 `
+
+
+
+
