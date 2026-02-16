cd3105677462cc4fd9af0b785cc29c053e620872
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-02-16 20:13:34 -0300

    refactor: modificando config

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 export const Config = {
   AI: {
     URL: process.env.IA_URL ?? "http://localhost:11434/api/generate",
-    MODEL: process.env.IA_MODEL ?? "qwen2.5:7b",
+    MODEL: process.env.IA_MODEL ?? "ebic",
   },
   API: {
     HOST: process.env.HOST ?? "http://localhost",
